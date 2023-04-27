2f5a7920ede0bc3c3e0e8b33a3882e73d5ecb086
KurtYoon <akakertcobain@naver.com>
2023-04-27 13:55:28 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')  # default URL
+def main():
+   return render_template('main.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = dict()
+      
+      result['Name'] = request.form.get('name')
+
+      result['Number'] = request.form.get('number')
+      
+      result['Major'] = request.form.get('Major')
+      
+      result['Email'] = request.form.get('email_id') + '@' + request.form.get('email_addr')
+
+      result['Gender'] = request.form.get('Gender')
+      
+      result['languages'] = ', '.join(request.form.getlist('chkbox'))
+      
+      return render_template("result.html", result = result)
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=8000, debug = True)
